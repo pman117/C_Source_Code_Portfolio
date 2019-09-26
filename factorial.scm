e2aaae9bbcd6faea542af9c5ef630665d529eb2f
Partha Ray <partha@partha-ThinkPad-W541>
2019-09-26 12:15:00 -0700

    change

--- factorial.scm
+++ factorial.scm
@@ -10,7 +10,14 @@
 ;substitution model 
 ;recursively apply the substitution model in applicative order:   evaluate sub expressions -> then substitute + apply the procedure
 ;recursion ->  wishful thinking -> smaller instances than size n
-;decomposing a problem(recursive algorithm) -> wishful thinking + convert solution to desired solution -> test + base case + recursive case(wishful thinking)
+;induction -> start with base case n
+;decomposing a problem(recursive algorithm) -> wishful thinking + convert solution to desired solution(ingenuity) -> test + base case + recursive case(wishful thinking)
+;process evolution -> demands on computational resources(space(number of deferred operations) + time(number of primitive steps))
+;space -> grows linearly(1x) with respect to size of said argument (each increase in argument size -> + 1constant amount of space requirement)
+;time -> grows linearly(2x) with respect to size of said argument (each increase in argument size -> + 2constant amount of time requirement)
+;orders of growth process 
+
+
 
 
 
